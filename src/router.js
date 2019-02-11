a8e170e7c6a855647dac99d23218df35be3ab897
fanqidong <1425434413@qq.com>
2019-02-11 10:59:20 +0800

    add

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path:'/',
+            redirect:'/home'
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: ()=>import('./views/Home.vue')
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+        }
+    ]
+})
---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      23132
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  background: red;
+  box-shadow: 10px 10px 10px #000;
+}
+</style>
+
+
