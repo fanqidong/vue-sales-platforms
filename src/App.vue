640a45a7ecdc6569dcee526651c1fd9f6843a528
fanqidong <1425434413@qq.com>
2019-02-11 11:58:39 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="app">
+    <div class="app-header">
+      <div class="app-nav clearfix">
+        <router-link :to="{path:'/'}" class="app-logo f-l">
+          <img src="./assets/logo.png" alt="logo">
+        </router-link>
+        <ul class="nav-list f-r">
+          <li>登录</li>
+          <li>注册</li>
+          <li>关于</li>
+        </ul>
+      </div>
     </div>
-    <router-view/>
+    <div class="app-content">
+      <keep-alive>
+        <router-view/>
+      </keep-alive>
+    </div>
+    <div class="app-footer">© 2019 All Rights Reserved.fanqidong</div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+@import url("./assets/scss/reset.css");
+.app {
+  &-header {
+    background: #363636;
+    color: #b2b2b2;
+    height: 80px;
+    line-height: 80px;
+  }
+  &-nav {
+    width: 1200px;
+    margin: auto;
+  }
+  &-logo {
+    display: block;
+    width: 80px;
+    img {
+      display: block;
+      width: 100%;
+    }
+  }
+  .nav-list {
+    li {
+      float: left;
+    }
+  }
 }
 </style>
+
